81cd0c29c6ac016d95d1c6e031448eccf3cba1a4
Gabriel <gabrielholanda18@hotmail.com>
2026-07-16 17:56:43 -0300

    wip(score): ScoreService com construtor recebendo lista de RegraScore

--- servico-score/src/main/java/com/desafio/nttdata/servicoscore/service/ScoreService.java
+++ servico-score/src/main/java/com/desafio/nttdata/servicoscore/service/ScoreService.java
@@ -1,8 +1,19 @@
 package com.desafio.nttdata.servicoscore.service;
 
+import com.desafio.nttdata.servicoscore.dto.ScoreResponseDTO;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class ScoreService {
+
+    private final Map<TipoCliente, RegraScore> regras;
+
+    public ScoreService(List<RegraScore> regrasDisponiveis) {
+        this.regras = regrasDisponiveis.stream()
+                .collect(Collectors.toMap(RegraScore::getTipoSuportado, Function.identity()));
+    }
 
 }
