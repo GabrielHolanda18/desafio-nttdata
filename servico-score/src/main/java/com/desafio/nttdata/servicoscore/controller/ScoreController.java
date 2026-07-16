66b96b8fb88ae80791f700c628e36849d4b5430d
Gabriel <gabrielholanda18@hotmail.com>
2026-07-16 17:45:18 -0300

    wip(score): inicia ScoreController (construtor com injecao do ScoreService) e cria classe ScoreService

---
+++ servico-score/src/main/java/com/desafio/nttdata/servicoscore/controller/ScoreController.java
@@ -0,0 +1,24 @@
+package com.desafio.nttdata.servicoscore.controller;
+
+import com.desafio.nttdata.servicoscore.service.ScoreService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/score")
+@Tag(name = "Score de Credito", description = "Calculo de score para clientes PF e PJ")
+public class ScoreController {
+
+    private final ScoreService scoreService;
+
+    public ScoreController(ScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
+
+}
---
+++ servico-score/src/main/java/com/desafio/nttdata/servicoscore/service/ScoreService.java
@@ -0,0 +1,8 @@
+package com.desafio.nttdata.servicoscore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScoreService {
+
+}
