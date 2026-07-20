90835f202a7912be1f81e35688b56a8b17048f2d
Gabriel <gabrielholanda18@hotmail.com>
2026-07-20 16:40:11 -0300

    feat(emprestimo): habilita @EnableFeignClients na classe principal

--- servico-emprestimo/src/main/java/com/desafio/nttdata/servicoemprestimo/ServicoEmprestimoApplication.java
+++ servico-emprestimo/src/main/java/com/desafio/nttdata/servicoemprestimo/ServicoEmprestimoApplication.java
@@ -2,8 +2,10 @@ package com.desafio.nttdata.servicoemprestimo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ServicoEmprestimoApplication {
 
 	public static void main(String[] args) {
